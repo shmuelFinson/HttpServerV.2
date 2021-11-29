56b8f5d03a402dd45fdd884ad21f168cf510290a
Sam Finson <sfinson@circuitdataanalytics.com>
2021-11-29 21:41:26 +0200

    Add ThreadPool

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,9 +1,14 @@
 import com.sun.net.httpserver.HttpServer;
 import config.Configuration;
 import config.ConfigurationManager;
+import http.EchoGetHandler;
+import http.EchoHeaderHandler;
+import http.EchoPostHandler;
+import http.RootHandler;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 
 public class Server {
 
@@ -13,14 +18,13 @@ public class Server {
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
         HttpServer server = HttpServer.create(new InetSocketAddress(conf.getPort()), 0);
-//        System.out.println("server started at " + port);
-//        server.createContext("/", new RootHandler());
-//        server.createContext("/echoHeader", new EchoHeaderHandler());
-//        server.createContext("/echoGet", new EchoGetHandler());
-//        server.createContext("/echoPost", new EchoPostHandler());
-//        server.setExecutor(null);
-//        server.start();
-
+        System.out.println("server started at " + conf.getPort());
+        server.createContext("/", new RootHandler());
+        server.createContext("/echoHeader", new EchoHeaderHandler());
+        server.createContext("/echoGet", new EchoGetHandler());
+        server.createContext("/echoPost", new EchoPostHandler());
+        server.setExecutor(Executors.newCachedThreadPool());
+        server.start();
 
     }
 
